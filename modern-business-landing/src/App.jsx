eea22ac25d52e1e342085d1c6045267cc2a6b933
Hermi-git <andargiehermela1@gmail.com>
2025-04-07 22:18:58 +0300

    complete hero section page

--- modern-business-landing/src/App.jsx
+++ modern-business-landing/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
-    <>
+    <main className='relative min-h-screen overflow-x-hidden'>
       <Navbar />
-    </>
+      <Hero/>
+    </main>
   )
 }
 
